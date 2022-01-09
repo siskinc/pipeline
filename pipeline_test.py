da503ff256f91353219b49d3659093f2ef2bef62
daryl <susecjh@gmail.com>
2022-01-09 17:06:18 +0800

    Feature: add a task with graph test

--- pipeline_test.py
+++ pipeline_test.py
@@ -1,3 +1,4 @@
+from task import TaskFactory
 import unittest
 
 from pipeline import Pipeline
@@ -22,6 +23,30 @@ class TestPipeline(unittest.IsolatedAsyncioTestCase):
             func_a: [func_b],
             func_b: [],
             func_c: [],
+        }
+        p = Pipeline(graph)
+        await p.run({}, {})
+
+    async def test_run_with_task(self):
+        handle1_code_str = """
+async def handle(task_data, context):
+    print("handle1")
+"""
+        handle2_code_str = """
+async def handle(task_data, context):
+    print("handle2")
+"""
+        handle3_code_str = """
+async def handle(task_data, context):
+    print("handle3")
+"""
+        handle1_func = TaskFactory.create_task_func(handle1_code_str)
+        handle2_func = TaskFactory.create_task_func(handle2_code_str)
+        handle3_func = TaskFactory.create_task_func(handle3_code_str)
+        graph = {
+            handle1_func: [handle2_func],
+            handle2_func: [],
+            handle3_func: [],
         }
         p = Pipeline(graph)
         await p.run({}, {})
